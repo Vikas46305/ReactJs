08ce1c732ca8386627ab059851ed72a6a2b99ad4
Vikas Sharma <vikassharma46305@gmail.com>
2024-11-06 07:50:37 +0530

    Dev.Vikas V1

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,6 +1,53 @@
-function Footer(){
-    return(
-        <h1 className="text-2xl font-bold flex justify-center">Footer Section</h1>
-    )
+function Footer() {
+  return (
+    <div className="flex justify-center flex-wrap max-md:flex-col text-sm mt-5">
+      <div className="w-1/4 px-5 max-md:w-full max-md:px-10">
+        <h1 className="text-2xl font-bold my-2">About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+          veniam necessitatibus tempora perspiciatis excepturi adipisci saepe
+          dolor nesciunt cupiditate itaque?
+        </p>
+      </div>
+
+      <div className="w-1/4 px-5 max-md:w-full max-md:px-10">
+        <h1 className="text-2xl font-bold my-2">Contact</h1>
+        <p>
+          <i className="ri-drag-move-2-line text-lg mr-3"></i>
+          IIMT University Ganga Nagar Meerut Uttar Pradesh 250001
+        </p>
+        <p>
+          <i className="ri-smartphone-line text-lg mr-3"></i>
+          8795915044
+        </p>
+        <p>
+          <i className="ri-mail-fill text-lg mr-3"></i>
+          vikassharma46305@gmail.com
+        </p>
+      </div>
+
+      <div className="w-1/4 px-5 max-md:w-full max-md:px-10">
+        <h1 className="text-2xl font-bold my-2">Categories</h1>
+        <div className="flex flex-col gap-2">
+          <p>Headphone</p>
+          <p>Headphone</p>
+          <p>Headphone</p>
+          <p>Headphone</p>
+        </div>
+      </div>
+
+      <div className="w-1/4 px-5 max-md:w-full max-md:px-10 mb-5">
+        <h1 className="text-2xl font-bold my-5">Pages</h1>
+        <div className="flex flex-col gap-3 ">
+          <p>Home</p>
+          <p>About</p>
+          <p>Privacy Policy</p>
+          <p>Returns</p>
+          <p>Trems & Conditions</p>
+          <p>Contact Us</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Footer;
--- src/Component/NewsLetter.jsx
+++ src/Component/NewsLetter.jsx
@@ -1,9 +1,39 @@
-
-function NewsLetter(){
-    return(
-        <div style={{}}>
-
-        </div>
-    )
+function NewsLetter() {
+  return (
+    <div className="bg-[url(../news.jpg)] p-10 bg-cover bg-center bg-no-repeat text-center flex flex-col gap-3">
+      <h1 className="text-xl">NEWSLETTER</h1>
+      <h1 className="text-2xl font-semibold">
+        SIGN UP FOR LATEST UPDATES AND OFFERS
+      </h1>
+      <div className="flex justify-center items-center gap-2">
+        <input
+          type="text"
+          className="w-[30%] py-2 border pl-2 rounded max-md:w-[80%]"
+          placeholder="email"
+        />
+        <button className="px-3 py-2 bg-purple-700 hover:bg-purple-950 hover:text-white rounded-sm">
+          Subscribe
+        </button>
+      </div>
+      <p>Will be used in accordance with our Privacy Policy</p>
+      <div className="text-3xl flex justify-center items-center gap-1">
+        <p>
+          <i className="ri-facebook-circle-fill hover:bg-gray-300 rounded-full p-1"></i>
+        </p>
+        <p>
+          <i className="ri-twitter-x-fill hover:bg-gray-300 rounded-full p-1"></i>
+        </p>
+        <p>
+          <i className="ri-instagram-line hover:bg-gray-300 rounded-full p-1"></i>
+        </p>
+        <p>
+          <i className="ri-linkedin-fill hover:bg-gray-300 rounded-full p-1"></i>
+        </p>
+        <p>
+          <i className="ri-telegram-fill hover:bg-gray-300 rounded-full p-1"></i>
+        </p>
+      </div>
+    </div>
+  );
 }
 export default NewsLetter;
